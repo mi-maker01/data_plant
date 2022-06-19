0a1fec0c41d77ead1944a66b9e26cf1b9d30b569
mi-maker01 <106691573+mi-maker01@users.noreply.github.com>
2022-06-20 00:52:22 +0900

    Update test.py

--- test.py
+++ test.py
@@ -57,7 +57,7 @@ if selector=="ヒストグラム":
         dosu_num=0
         
         for t in t_list:
-            x,y=[]
+            x,y=0
             y_num=df[(df["図番"]==z)&(df["工程コード"]==k)&(df["担当コード"] == t)]
             #y軸の上限値
             x,y,_= plt.hist(y_num)
