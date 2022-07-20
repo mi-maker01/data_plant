a11bc3b8d424aa3f45ced2f57cf304e34f945383
mi-maker01 <106691573+mi-maker01@users.noreply.github.com>
2022-07-20 22:50:34 +0900

    Update test.py

--- test.py
+++ test.py
@@ -351,8 +351,7 @@ elif selector=="ガントチャート":
                     st.dataframe(d_num)
                     #描画領域を用意する
                     fig, ax = plt.subplots()
-                    
-                    fig= px.timeline(d_num, x_start="工程開始時間", x_end="工程完了時間",text="処理時間",y="製造番号",title="設備の稼働状況見える化")
-                    fig.update_traces(textposition='inside', orientation="h")
+                    ax = px.timeline(d_num, x_start="工程開始時間", x_end="工程完了時間",text="処理時間",y="製造番号",title="設備の稼働状況見える化")
+                    ax.update_traces(textposition='inside', orientation="h")
                     fig.savefig("img.png")
                     st.pyplot(fig)
