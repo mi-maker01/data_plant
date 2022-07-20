65e56d8e9da688f0ebbcd423cfdbb3adb2b5086c
mi-maker01 <106691573+mi-maker01@users.noreply.github.com>
2022-07-20 18:55:29 +0900

    Update test.py

--- test.py
+++ test.py
@@ -352,6 +352,6 @@ elif selector=="ガントチャート":
                     fig = plt.figure(figsize=(12,9))
                     ax = fig.add_subplot()
                     
-                    px.timeline(d_num, x_start="工程開始時間", x_end="工程完了時間",text="処理時間",y="製造番号",title="設備の稼働状況見える化")
+                    st.timeline(d_num, x_start="工程開始時間", x_end="工程完了時間",text="処理時間",y="製造番号",title="設備の稼働状況見える化")
 #                     ax.update_traces(textposition='inside', orientation="h")
                     st.pyplot(fig)
