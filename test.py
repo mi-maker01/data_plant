f8c94af235e457f576ac79e922d9b6382f7302e4
mi-maker01 <106691573+mi-maker01@users.noreply.github.com>
2022-07-22 00:02:38 +0900

    Update test.py

--- test.py
+++ test.py
@@ -140,10 +140,11 @@ if selector=="ヒストグラム（工程）":#=================================
             
             ax.hist(dd,bins=10,range=(lower_num2,upper_num2),rwidth=dif_num2/10)
             
-            left_column, right_column = st.columns(2)
+            
             # Matplotlib の Figure を指定して可視化する
             st.write("---------------このグラフのデータ個数：",len(dd),"-------------担当コード：",i,"-----------------------")
             st.plotly_chart(fig)
+            left_column, right_column = st.columns(2)
             left_column.pyplot(fig)
         #===============================================================================================================================(ヒストグラムの設定)
 #担当者の画面
