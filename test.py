c55477a928832f6ffc721ee2ec660c5b01503113
mi-maker01 <106691573+mi-maker01@users.noreply.github.com>
2022-08-02 14:53:00 +0900

    Update test.py

--- test.py
+++ test.py
@@ -426,6 +426,8 @@ elif selector=="折れ線グラフ":
         for s in s_num:        
             sei_num=d_num[d_num["製造番号"]==s]
             sei_num=sei_num.sort_values(["工程開始時間"])
+            sei_num=sei_num.reset_index()
+            
             st.dataframe(sei_num)
             fig = go.Figure(px.line(sei_num, x="工程開始時間", y="工程コード", markers=True))
             st.plotly_chart(fig)
