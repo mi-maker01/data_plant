5c2bea3af5ce4796abc46a9c13a4cfb63efea86e
mi-maker01 <106691573+mi-maker01@users.noreply.github.com>
2022-08-08 21:01:32 +0900

    Update test.py

--- test.py
+++ test.py
@@ -243,8 +243,10 @@ elif selector=="（C）同一行程内のばらつき把握_ヒストグラム":
         fig = go.Figure(px.box(s_num))
         st.plotly_chart(fig, use_container_width=True)
         
-        st.dataframe(data_num['processing_time'].describe().T)#データの詳細データ
-                
+        syosai_num=data_num['processing_time'].describe()#データの詳細データ
+        syosai_num = pd.DataFrame(syosai_num)
+        st.write(syosai_num.T)
+        
         q1=data_num['processing_time'].describe().loc['25%']#第一四分位範囲
         q3=data_num['processing_time'].describe().loc['75%']#第三四分位範囲
                 
