bbac5253a686fec03d2b4f62c4531467f10e746e
mi-maker01 <106691573+mi-maker01@users.noreply.github.com>
2022-08-08 16:42:48 +0900

    Update test.py

--- test.py
+++ test.py
@@ -98,43 +98,97 @@ elif selector=="（A-2）各工程各日の実績ガントチャート":
                 fig.update_traces(textposition='inside', orientation="h")
                 fig.update_yaxes(autorange='reversed')
                 st.plotly_chart(fig)
-            
-elif selector=="折れ線グラフ":
-    day_num = sorted(list(set(df["工程完了日"])))
-    d = st.selectbox(
-         "工程完了日",
-         (day_num))
-    d_num=df[(df["工程完了日"]==d)]
-    d_num=d_num.sort_values(["工程開始時間"])
-    s_num = sorted(list(set(d_num["製造番号"])))
+
+elif selector=="（B）同一人物の同一行程でのばらつきの把握_ヒストグラム":
+    #標準時間の取り込み
+    st.title("標準時間ファイル")
+    uploaded_file=st.file_uploader("標準時間の取り込み",type="xlsx")
+    if uploaded_file is not None:
+        df_time=pd.read_excel(uploaded_file)
+    base_time = pd.to_datetime('00:00:0', format='%M:%S:%f')
+    df_time['標準時間']=pd.to_datetime(df_time['標準時間'], format='%M:%S:%f') - base_time
+    df_time['標準時間']=df_time["標準時間"].dt.total_seconds()
     
+    #担当の選択
+    t_list = sorted(list(set(df["担当コード"])))
+    t = st.selectbox(
+         "担当コード",
+         (t_list))
+    x_num=df[(df["担当コード"]==t)]#dfからzで選んだ図番のデータ
+    k_list = sorted(list(set(x_num["工程コード"])))
+    z_list = sorted(list(set(x_num["図番"])))
+    
+    
+    #データ分析開始
     answer = st.button('分析開始')
     if answer == True:
-        for s in s_num:        
-            sei_num=d_num[d_num["製造番号"]==s]
-            sei_num=sei_num.sort_values(["工程開始時間"])
-            sei_num["工程開始時間"] = pd.to_datetime(sei_num["工程開始時間"], format="%H:%M:%S")
-            sei_num["工程完了時間"] = pd.to_datetime(sei_num["工程完了時間"], format="%H:%M:%S")
-            sei_num=sei_num.reset_index()
+        
+        for z in z_list:
+            for k in k_list:
+                data_num=df[(df["図番"]==z)&(df["工程コード"]==k)]
+                dosu_num=0
+                
+                y_num=df[(df["図番"]==z)&(df["工程コード"]==k)&(df["担当コード"] == t)]
+                y_num=y_num["処理時間"]
+                if len(y_num)==0:
+                    break
+                #y軸の上限値
+                x,y,_= plt.hist(y_num)
+                if dosu_num<max(x):#tが2個以上の時に比較する
+                    dosu_num=max(x)
+                    
+                data_num=data_num.rename(columns={'処理時間': 'processing_time'}) 
+                s_num=data_num['processing_time']
+                    
+                q1=data_num['processing_time'].describe().loc['25%']#第一四分位範囲
+                q3=data_num['processing_time'].describe().loc['75%']#第三四分位範囲
+                
+                iqr=q3-q1#四分位範囲
+                upper_num=q3+(1.5*iqr)#上限
+                lower_num=q1-(1.5*iqr)#下限
+                upper_num2=round(upper_num) #きりあげ
+                lower_num2=math.floor(lower_num)#きりおとし
+                dif_num=upper_num2-lower_num2#差
+                    
+                if dif_num%10!=0:#もし切り上げ切り落としした差が10で割れなかった
+                    dif_num2=math.ceil((dif_num/10))*10
+                dif_num3=(dif_num2-dif_num)/2
+                upper_num2=upper_num2+dif_num3
+                lower_num2=lower_num2-dif_num3
+                
+                hazure=data_num[data_num["processing_time"]<=upper_num]
+                hazure=hazure[hazure["processing_time"]>=lower_num]
+        
+                    #ヒストグラムの作成
+                #データの整理
+                scores=hazure[(hazure["図番"]==z)&(hazure["工程コード"]==k)&(hazure["担当コード"]==t)]#選択したデータ
+                y_scores=df_time[(df_time["図番"]==z)&(df_time["工程コード"] ==k)]
+                hyozyun=y_scores["標準時間"]
+                dd=scores["processing_time"]#選択したデータの処理時間
             
-            gura_num = pd.DataFrame()
-            s_num = pd.DataFrame()
-            for i in range(len(sei_num)):
-                sei_num['工程時間'] = sei_num["工程開始時間"]
-                s_num = sei_num.iloc[i]
-                gura_num=gura_num.append(s_num)
-                sei_num['工程時間'] = sei_num["工程完了時間"]
-                s_num = sei_num.iloc[i]
-                gura_num=gura_num.append(s_num)
-           
-            gura_num=gura_num.sort_values(["工程時間"])
-            st.dataframe(gura_num)
-            st.write("--------------------------")
-            fig = go.Figure(px.line(gura_num, x="工程時間", y="工程コード", markers=True))
-            fig.update_yaxes(autorange='reversed')
-            st.plotly_chart(fig)
-#ヒストグラムの画面
-if selector=="ヒストグラム（工程）":#===============================================================================================
+                #描画領域を用意する
+                fig = plt.figure()
+                ax = fig.add_subplot()
+
+                plt.xlim([0,upper_num2])                        # X軸範囲
+                plt.ylim([0,dosu_num+10])                      # Y軸範囲
+                ax.set_title("chart")
+                ax.set_xlabel("time")                # x軸ラベル
+                plt.ylabel("count")               # y軸ラベル
+                plt.grid(True)
+                plt.axvline(x=int(hyozyun),color = "crimson")#標準時間の表記（赤軸）
+                plt.xticks(np.arange(lower_num2, upper_num2,dif_num2/10))
+                
+
+                ax.hist(dd,bins=10,range=(lower_num2,upper_num2),rwidth=dif_num2/10)
+                # Matplotlib の Figure を指定して可視化する
+                st.write("---------------工程コード:",k,"-------------図番:",z,"------------データの数:",len(scores),"------------------")
+                left_column, right_column = st.columns(2)
+                right_column.plotly_chart(fig)
+                left_column.pyplot(fig)
+                
+#=======================================================================================================================================================
+if selector=="（C）同一行程内のばらつき把握_ヒストグラム":
     
     #標準時間の取り込み
     st.title("標準時間ファイル")
@@ -255,10 +309,46 @@ if selector=="ヒストグラム（工程）":#=================================
             st.write("---------------このグラフのデータ個数：",len(dd),"-------------担当コード：",i,"-----------------------")
             left_column, right_column = st.columns(2)
             right_column.plotly_chart(fig)
-            left_column.pyplot(fig)
-        #===============================================================================================================================(ヒストグラムの設定)
+            left_column.pyplot(fig)            
+
+elif selector=="（D）一つの製品の総社内滞在時間の把握_折れ線グラフ":
+    day_num = sorted(list(set(df["工程完了日"])))
+    d = st.selectbox(
+         "工程完了日",
+         (day_num))
+    d_num=df[(df["工程完了日"]==d)]
+    d_num=d_num.sort_values(["工程開始時間"])
+    s_num = sorted(list(set(d_num["製造番号"])))
+    
+    answer = st.button('分析開始')
+    if answer == True:
+        for s in s_num:        
+            sei_num=d_num[d_num["製造番号"]==s]
+            sei_num=sei_num.sort_values(["工程開始時間"])
+            sei_num["工程開始時間"] = pd.to_datetime(sei_num["工程開始時間"], format="%H:%M:%S")
+            sei_num["工程完了時間"] = pd.to_datetime(sei_num["工程完了時間"], format="%H:%M:%S")
+            sei_num=sei_num.reset_index()
+            
+            gura_num = pd.DataFrame()
+            s_num = pd.DataFrame()
+            for i in range(len(sei_num)):
+                sei_num['工程時間'] = sei_num["工程開始時間"]
+                s_num = sei_num.iloc[i]
+                gura_num=gura_num.append(s_num)
+                sei_num['工程時間'] = sei_num["工程完了時間"]
+                s_num = sei_num.iloc[i]
+                gura_num=gura_num.append(s_num)
+           
+            gura_num=gura_num.sort_values(["工程時間"])
+            st.dataframe(gura_num)
+            st.write("--------------------------")
+            fig = go.Figure(px.line(gura_num, x="工程時間", y="工程コード", markers=True))
+            fig.update_yaxes(autorange='reversed')
+            st.plotly_chart(fig)
+
+#=======================================================================================================================================
 #担当者の画面
-elif selector=="担当者":
+elif selector=="担当者別作業時間統計量":
     t_list = sorted(list(set(df["担当コード"])))
     t = st.selectbox(
          "担当コード",
@@ -274,7 +364,7 @@ elif selector=="担当者":
         st.table(pvit)
         
 #図番の画面
-elif selector=="図番":
+elif selector=="図番別作業時間統計量":
     z_list = sorted(list(set(df["図番"])))
     z = st.selectbox(
          "図番",
@@ -290,7 +380,7 @@ elif selector=="図番":
         st.table(pvit)
         
 #工程の画面
-elif selector=="工程":
+elif selector=="工程別作業時間統計量":
     k_list = sorted(list(set(df["工程コード"])))
     k = st.selectbox(
          "工程コード",
@@ -305,98 +395,8 @@ elif selector=="工程":
         st.dataframe(pvit)
         st.table(pvit)
  
-#工程の画面
-elif selector=="ヒストグラム（担当コード）":
-    #標準時間の取り込み
-    st.title("標準時間ファイル")
-    uploaded_file=st.file_uploader("標準時間の取り込み",type="xlsx")
-    if uploaded_file is not None:
-        df_time=pd.read_excel(uploaded_file)
-    base_time = pd.to_datetime('00:00:0', format='%M:%S:%f')
-    df_time['標準時間']=pd.to_datetime(df_time['標準時間'], format='%M:%S:%f') - base_time
-    df_time['標準時間']=df_time["標準時間"].dt.total_seconds()
-    
-    #担当の選択
-    t_list = sorted(list(set(df["担当コード"])))
-    t = st.selectbox(
-         "担当コード",
-         (t_list))
-    x_num=df[(df["担当コード"]==t)]#dfからzで選んだ図番のデータ
-    k_list = sorted(list(set(x_num["工程コード"])))
-    z_list = sorted(list(set(x_num["図番"])))
-    
-    
-    #データ分析開始
-    answer = st.button('分析開始')
-    if answer == True:
-        
-        for z in z_list:
-            for k in k_list:
-                data_num=df[(df["図番"]==z)&(df["工程コード"]==k)]
-                dosu_num=0
-                
-                y_num=df[(df["図番"]==z)&(df["工程コード"]==k)&(df["担当コード"] == t)]
-                y_num=y_num["処理時間"]
-                if len(y_num)==0:
-                    break
-                #y軸の上限値
-                x,y,_= plt.hist(y_num)
-                if dosu_num<max(x):#tが2個以上の時に比較する
-                    dosu_num=max(x)
-                    
-                data_num=data_num.rename(columns={'処理時間': 'processing_time'}) 
-                s_num=data_num['processing_time']
-                    
-                q1=data_num['processing_time'].describe().loc['25%']#第一四分位範囲
-                q3=data_num['processing_time'].describe().loc['75%']#第三四分位範囲
-                
-                iqr=q3-q1#四分位範囲
-                upper_num=q3+(1.5*iqr)#上限
-                lower_num=q1-(1.5*iqr)#下限
-                upper_num2=round(upper_num) #きりあげ
-                lower_num2=math.floor(lower_num)#きりおとし
-                dif_num=upper_num2-lower_num2#差
-                    
-                if dif_num%10!=0:#もし切り上げ切り落としした差が10で割れなかった
-                    dif_num2=math.ceil((dif_num/10))*10
-                dif_num3=(dif_num2-dif_num)/2
-                upper_num2=upper_num2+dif_num3
-                lower_num2=lower_num2-dif_num3
-                
-                hazure=data_num[data_num["processing_time"]<=upper_num]
-                hazure=hazure[hazure["processing_time"]>=lower_num]
-        
-                    #ヒストグラムの作成
-                #データの整理
-                scores=hazure[(hazure["図番"]==z)&(hazure["工程コード"]==k)&(hazure["担当コード"]==t)]#選択したデータ
-                y_scores=df_time[(df_time["図番"]==z)&(df_time["工程コード"] ==k)]
-                hyozyun=y_scores["標準時間"]
-                dd=scores["processing_time"]#選択したデータの処理時間
-            
-                #描画領域を用意する
-                fig = plt.figure()
-                ax = fig.add_subplot()
-
-                plt.xlim([0,upper_num2])                        # X軸範囲
-                plt.ylim([0,dosu_num+10])                      # Y軸範囲
-                ax.set_title("chart")
-                ax.set_xlabel("time")                # x軸ラベル
-                plt.ylabel("count")               # y軸ラベル
-                plt.grid(True)
-                plt.axvline(x=int(hyozyun),color = "crimson")#標準時間の表記（赤軸）
-                plt.xticks(np.arange(lower_num2, upper_num2,dif_num2/10))
-                
-
-                ax.hist(dd,bins=10,range=(lower_num2,upper_num2),rwidth=dif_num2/10)
-                # Matplotlib の Figure を指定して可視化する
-                st.write("---------------工程コード:",k,"-------------図番:",z,"------------データの数:",len(scores),"------------------")
-                left_column, right_column = st.columns(2)
-                right_column.plotly_chart(fig)
-                left_column.pyplot(fig)
-                
-          #===============================================================================================================================(ヒストグラムの設定)
-#工程の画面
-elif selector=="工程量":
+ #===============================================================================================================================================
+elif selector=="各人の工程量":
     num=df[["図番","製造番号","工程コード","担当コード","工程開始時間","工程完了日","工程完了時間"]]
     t_list = sorted(list(set(df["担当コード"])))
     
@@ -421,30 +421,3 @@ elif selector=="工程量":
 
         fig = go.Figure(go.Pie(values=z_num,labels=k_num,hole=.3))
         st.plotly_chart(fig, use_container_width=True)
-
-elif selector=="滞在時間":
-    k_list = sorted(list(set(df["工程完了日"])))
-    k = st.selectbox(
-         "工程完了日",
-         (k_list))
-    
-    z_num=df[(df["工程完了日"]==k)]
-    s_list = sorted(list(set(z_num["製造番号"])))
-    s = st.selectbox(
-         "製造番号",
-         (s_list))
-    s_num=z_num[z_num["製造番号"]==s]
-    s_num=s_num.sort_values(['工程完了日','工程開始時間'])
-    
-    d_num1=s_num
-    d_num2=s_num[s_num["工程コード"]=="P028"]
-    
-    
-    #データ分析開始
-    answer = st.button('分析開始')
-    if answer == True:
-        st.write(d_num1)
-        st.write(d_num2)
-
-
-   
