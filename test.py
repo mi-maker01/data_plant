3334e0a1559137ec1c2319ff94a811aa34258749
mi-maker01 <106691573+mi-maker01@users.noreply.github.com>
2022-08-17 14:55:01 +0900

    Update test.py

--- test.py
+++ test.py
@@ -428,8 +428,8 @@ elif selector=="（E）各人の工程量":
         k_list = sorted(list(set(t_num["工程名称"])))
         for k in k_list:
             k_num=t_num[(t_num["工程名称"]==k)]
-#             bar_num.loc[k,t]=round(100 * len(k_num) / len(t_num), 1)
-    
+            
+            bar_num.append({'担当者': t, '工程名称': k, '%': round(100 * len(k_num) / len(t_num), 1)}
     st.dataframe(bar_num1)
     answer = st.button('分析開始')
     if answer == True:
