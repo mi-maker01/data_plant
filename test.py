f52a3efaa3bc1a7c2e44bee105589bf5c599f2d9
mi-maker01 <106691573+mi-maker01@users.noreply.github.com>
2022-08-18 17:22:04 +0900

    Update test.py

--- test.py
+++ test.py
@@ -109,9 +109,12 @@ elif selector=="（B）同一人物の同一行程でのばらつきの把握_�
     if uploaded_file is not None:
         df_time=pd.read_excel(uploaded_file)
     base_time = pd.to_datetime('00:00:0', format='%M:%S:%f')
-    df_time['標準時間']=pd.to_datetime(df_time['標準時間'], format='%M:%S:%f') - base_time
-    df_time['標準時間']=df_time["標準時間"].dt.total_seconds()
+    df_time['標準時間1']=pd.to_datetime(df_time['標準時間1'], format='%M:%S:%f') - base_time
+    df_time['標準時間1']=df_time["標準時間1"].dt.total_seconds()
+    df_time['標準時間2']=pd.to_datetime(df_time['標準時間2'], format='%M:%S:%f') - base_time
+    df_time['標準時間2']=df_time["標準時間2"].dt.total_seconds()
     
+    st.write(df_time)
     #曜日の設定
     
     
