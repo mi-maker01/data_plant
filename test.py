56ca1f982d2e6589ca71d2bbc17fb510f2773cc4
mi-maker01 <106691573+mi-maker01@users.noreply.github.com>
2022-08-18 13:08:46 +0900

    Update test.py

--- test.py
+++ test.py
@@ -442,6 +442,6 @@ elif selector=="（E）各人の工程量":
         fig = go.Figure(px.bar(n_num,x="担当者",y="作成数",color="工程コード"))
         st.plotly_chart(fig, use_container_width=True)
         
-        fig = go.Figure(px.bar(bar_num,x="担当者",y="%",color="工程名称"))
+        fig = go.Figure(px.bar(bar_num1,x="担当者",y="%",color="工程名称"))
         left_column.plotly_chart(fig, use_container_width=True)
         
