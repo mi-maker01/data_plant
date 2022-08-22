b901ac028d6c457da38cac186d2a692a4e261838
mi-maker01 <106691573+mi-maker01@users.noreply.github.com>
2022-08-23 01:21:42 +0900

    Update test.py

--- test.py
+++ test.py
@@ -193,13 +193,10 @@ elif selector=="（B）同一人物の同一行程でのばらつきの把握_�
                 #ヒストグラムの作成
                 #データの整理
                 scores=hazure[(hazure["図番"]==z)&(hazure["工程名称"]==k)&(hazure["担当者"]==t)]#選択したデータ（外れ値）
-                y_scores=df_time[(df_time["図番"]==z)&(df_time["工程名称"]==k)]#選択したデータ（外れ値）
-                x_scores=df_time[(df_time["図番"]==z)&(df_time["工程名称"]==k)]#標準時間のデータ
+                y_scores=df_time[(df_time["図番"]==z)&(df_time["工程名称"]==k)]#標準時間のデータ
+                
                 hyozyun1=y_scores["標準時間1"]
                 hyozyun2=y_scores["標準時間2"]
-                st.write(scores)
-                st.write(y_scores)
-               
                 
                 dd=scores["processing_time"]#選択したデータの処理時間
             
@@ -213,8 +210,8 @@ elif selector=="（B）同一人物の同一行程でのばらつきの把握_�
                 ax.set_xlabel("time")                # x軸ラベル
                 plt.ylabel("count")               # y軸ラベル
                 plt.grid(True)
-#                 plt.axvline(x=hyozyun1,color = "crimson")#標準時間の表記（赤軸）
-#                 plt.axvline(x=int(hyozyun2),color = "Blue")#標準時間の表記（軸）
+                plt.axvline(x=hyozyun1,color = "crimson")#標準時間の表記（赤軸）
+                plt.axvline(x=int(hyozyun2),color = "Blue")#標準時間の表記（軸）
                 plt.xticks(np.arange(lower_num2, upper_num2,dif_num/10))
                 
 
