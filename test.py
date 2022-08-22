8a4d427861f6420af5fcd9d00714b83df3198ee2
mi-maker01 <106691573+mi-maker01@users.noreply.github.com>
2022-08-23 01:07:39 +0900

    Update test.py

--- test.py
+++ test.py
@@ -197,7 +197,7 @@ elif selector=="（B）同一人物の同一行程でのばらつきの把握_�
                 x_scores=df_time[(df_time["図番"]==z)&(df_time["工程名称"] ==k)]#標準時間のデータ
 #                 hyozyun1=y_scores["標準時間1"]
 #                 hyozyun2=y_scores["標準時間2"]
-                st.write(x_scores)
+                st.write(scores)
                
                 
                 dd=scores["processing_time"]#選択したデータの処理時間
