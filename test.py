362efb8c6d8ef0792146b2fa1219bba74afb5547
mi-maker01 <106691573+mi-maker01@users.noreply.github.com>
2022-08-22 23:37:27 +0900

    Update test.py

--- test.py
+++ test.py
@@ -76,7 +76,14 @@ if selector=="(A-1)各人各日の実績ガントチャート":
                     
                     fig = go.Figure(px.timeline(d_num, x_start="工程開始時間", x_end="工程完了時間",text="処理時間",y="担当者",color="工程名称",title="一日の稼働状況見える化"))
                     fig.update_traces(textposition='inside', orientation="h")
-#                     fig.update_yaxes(autorange='reversed')
+                    st.plotly_chart(fig)
+                    
+                    fig = go.Figure(px.timeline(d_num, x_start="工程開始時間", x_end="工程完了時間",text="処理時間",y="号機名称",color="工程名称",title="一日の稼働状況見える化"))
+                    fig.update_traces(textposition='inside', orientation="h")
+                    st.plotly_chart(fig)
+                    
+                    fig = go.Figure(px.timeline(d_num, x_start="工程開始時間", x_end="工程完了時間",text="処理時間",y="工程名称",color="担当者",title="一日の稼働状況見える化"))
+                    fig.update_traces(textposition='inside', orientation="h")
                     st.plotly_chart(fig)
                     #================================================================================================================================
 elif selector=="（A-2）各工程各日の実績ガントチャート":
