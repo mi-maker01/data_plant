8309241523e589c17f141080dc56c6111432d298
mi-maker01 <106691573+mi-maker01@users.noreply.github.com>
2022-08-23 14:38:55 +0900

    Update test.py

--- test.py
+++ test.py
@@ -452,7 +452,7 @@ elif selector=="（E）工程別作業時間統計量":
         
         graph_num=pd.DataFrame()
         list_1=sorted(list(set(df[num_1])))
-            st.write(list_1)
+        st.write(list_1)
 #         for hazure_num1 in list_1:
 #             x_num=df[(df[num_1]==hazure_num1)]
 #             list_2 = sorted(list(set(x_num[num_2]))
