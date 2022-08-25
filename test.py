09b7d62a56b89861581a15a0f6aaf1cfd42adc7a
mi-maker01 <106691573+mi-maker01@users.noreply.github.com>
2022-08-26 00:30:01 +0900

    Update test.py

--- test.py
+++ test.py
@@ -77,7 +77,7 @@ if selector=="(A-1)各人各日の実績ガントチャート":
                         for i in range(len(s_num)):
                             df2 = pd.DataFrame({"製造番号":s,"工程名称":"隙間時間","開始日時":sta_num[i], "完了日時":end_num[i]},index=['間の時間'])
                             graph_num=pd.concat([s_num, df2], axis=0)
-                        st.dataframe(sta_num)
+                        st.dataframe(graph_num_num)
                     
                     #描画領域を用意する
 #                     fig = plt.subplots()
