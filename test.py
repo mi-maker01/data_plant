6253375d791fe1ca761a0dbc449702643e50ff4f
mi-maker01 <106691573+mi-maker01@users.noreply.github.com>
2022-08-26 00:48:53 +0900

    Update test.py

--- test.py
+++ test.py
@@ -76,7 +76,7 @@ if selector=="(A-1)各人各日の実績ガントチャート":
                             end_num.append(row.完了日時)
 
                         for i in range(len(s_num)-1):
-                            df2 = pd.DataFrame({"製造番号":s,"工程名称":"隙間時間","開始日時":end_num[i], "完了日時":sta_num[i+1]},index=['間の時間'])
+                            df2 = pd.DataFrame({"製造番号":s,"工程名称":"隙間時間","開始日時":end_num[i], "完了日時":sta_num[i+1]},index=['time'])
                             graph_num=pd.concat([s_num, df2], axis=0)
                         st.write(graph_num)
                         
