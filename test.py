a2899ab982f1f3abe88209787577ff97c9710860
mi-maker01 <106691573+mi-maker01@users.noreply.github.com>
2022-08-26 00:43:40 +0900

    Update test.py

--- test.py
+++ test.py
@@ -77,9 +77,9 @@ if selector=="(A-1)各人各日の実績ガントチャート":
                         for i in range(len(s_num)-1):
                             df2 = pd.DataFrame({"製造番号":s,"工程名称":"隙間時間","開始日時":end_num[i], "完了日時":sta_num[i+1]},index=['間の時間'])
                             graph_num=pd.concat([s_num, df2], axis=0)
-                            fig = go.Figure(px.timeline(graph_num, x_start="開始日時", x_end="完了日時",y="担当者",color="工程名称",title="一日の各人の稼働状況"))
-                            fig.update_traces(textposition='inside', orientation="h")
-                            st.plotly_chart(fig)
+                        fig = go.Figure(px.timeline(graph_num, x_start="開始日時", x_end="完了日時",y="担当者",color="工程名称",title="一日の各人の稼働状況"))
+                        fig.update_traces(textposition='inside', orientation="h")
+                        st.plotly_chart(fig)
                     
                     #描画領域を用意する
 #                     fig = plt.subplots()
