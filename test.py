3536a3b3b9df5fbd0d3d384e0f85035091abaaea
mi-maker01 <106691573+mi-maker01@users.noreply.github.com>
2022-08-26 01:23:39 +0900

    Update test.py

--- test.py
+++ test.py
@@ -84,9 +84,9 @@ if selector=="(A-1)各人各日の実績ガントチャート":
                             df3.loc[0,'工程名称'] = "隙間時間"
                             df3.loc[0,'開始日時'] = end_num[i]+ datetime.timedelta(seconds=1)
                             df3.loc[0,'完了日時'] = sta_num[i+1]+ datetime.timedelta(seconds=-1)
-                            
                             t_num=pd.concat([t_num, df3], axis=0)
-                            
+                        
+                        t_num=t_num.sort_values("開始日時")    
                         st.write(t_num)
                         
                     #描画領域を用意する
