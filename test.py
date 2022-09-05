e331657d95eefd6ba371fb8498e907731dba07da
mi-maker01 <106691573+mi-maker01@users.noreply.github.com>
2022-09-05 14:25:51 +0900

    Update test.py

--- test.py
+++ test.py
@@ -412,6 +412,7 @@ elif selector=="（D）一つの製品の総社内滞在時間の把握":
         st.write(d_start)
         st.write(d_end)
         st.write(dt)
+        st.DataFrame(d_num)
 #         st.write("-----------------------------------------------------------------------------------")
 #         fig = go.Figure(px.timeline(d_num, x_start="開始日時", x_end="完了日時",text="処理時間",y="製造番号",color="工程名称",title="総社内滞在時間"))
 #         fig.update_traces(textposition='inside', orientation="h")
