a9d4aac78a59ae9b0648731990ae4c73dd2d95cd
mi-maker01 <106691573+mi-maker01@users.noreply.github.com>
2022-09-05 15:28:58 +0900

    Update test.py

--- test.py
+++ test.py
@@ -396,7 +396,8 @@ elif selector=="（D）一つの製品の総社内滞在時間の把握":
     dt= dt.days
     
     k_list = sorted(list(set(st.session_state.df["工程名称"])))
-    date_num = pd.DataFrame(index=k_list)
+    date_num = pd.DataFrame()
+    date_num.columns =　k_list
     d_num=st.session_state.df[(st.session_state.df["工程完了日"]==d_start)]
     for d in range(dt):#日のデータの追加文
         
