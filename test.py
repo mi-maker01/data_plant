9711c87ec38f94ba1f60f2b07d994807d774a01c
mi-maker01 <106691573+mi-maker01@users.noreply.github.com>
2022-09-23 15:07:19 +0900

    Update test.py

--- test.py
+++ test.py
@@ -91,7 +91,7 @@ if selector=="(A-1)各人各日の実績ガントチャート":
                             end_num.append(row.完了日時)
 
                         for i in range(len(t_num)-1):
-                            aki_time=+end_time[i]-st_time[i+1]
+                            aki_time+=end_time[i]-st_time[i+1]
                             
                             df3 =pd.DataFrame(columns =t_col )
                             df2 = pd.DataFrame({"担当者":t,"工程名称":"隙間時間","開始日時":end_num[i], "完了日時":sta_num[i+1]},index=['time'])
