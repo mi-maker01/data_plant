dd08985e61463c5db146211deae921d24671c133
mi-maker01 <106691573+mi-maker01@users.noreply.github.com>
2022-09-25 05:01:32 +0900

    Update test.py

--- test.py
+++ test.py
@@ -471,7 +471,7 @@ elif selector=="（D）一つの製品の総社内滞在時間の把握":
                 date_koutei_num=date_koutei_num.append(s_num.tail(1))
             num=pd.DataFrame(date_koutei_num.groupby(["工程名称"])['作成数'].agg(["count"]))
             st.write(d_start)
-            pvit=num.set_axis(["仕掛数"], axis=1)
+            pvit=num.set_axis([d_start], axis=1)
             st.write(pvit)
             
             d_num=d_num.append(kari_num)
