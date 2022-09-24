89d240e332b7b588528244fdace0993773661e6b
mi-maker01 <106691573+mi-maker01@users.noreply.github.com>
2022-09-25 02:14:40 +0900

    Update test.py

--- test.py
+++ test.py
@@ -601,7 +601,7 @@ elif selector=="（E）各人の工程量":
         
         #描画領域を用意する
         left_column, right_column = st.columns(2)
-        fig = go.Figure(px.bar(n_num,x="担当者",y="作成数",color="工程名称",text="図番",text="処理時間"))
+        fig = go.Figure(px.bar(n_num,x="担当者",y="作成数",color="工程名称",text="図番"))
         st.plotly_chart(fig, use_container_width=True)
         
         
