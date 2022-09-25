e3725561079265f70bdffde2b9de9499700f0eae
mi-maker01 <106691573+mi-maker01@users.noreply.github.com>
2022-09-25 14:46:35 +0900

    Update test.py

--- test.py
+++ test.py
@@ -613,15 +613,15 @@ elif selector=="（E）作業時間統計量":
         list_1=sorted(list(set(d_num[num_1])))
         for hazure_num1 in list_1:
             x_num=d_num[(d_num[num_1]==hazure_num1)]
-            x_num2=st.session_state.df_time[st.session_state.df_time([num_1]==hazure_num1)]
+            
             list_2=sorted(list(set(x_num[num_2])))
             for hazure_num2 in list_2:
                 y_num=x_num[(x_num[num_2]==hazure_num2)]
-                y_num2=x_num2[(x_num2[num_2]==hazure_num2)]
+                
                 list_3 = sorted(list(set(y_num[num_3])))
                 for hazure_num3 in list_3:
                     z_num=y_num[(y_num[num_3]==hazure_num3)]
-                    z_num2=y_num2[(y_num2[num_3]==hazure_num3)]
+                    
 
                     q1=z_num["処理時間"].describe().loc['25%']#第一四分位範囲
                     q3=z_num['処理時間'].describe().loc['75%']#第三四分位範囲
@@ -641,10 +641,10 @@ elif selector=="（E）作業時間統計量":
                     pvit.insert(0, '総件数', zentai_num)
                     
                    
-                    hyozyun1=z_num2["標準時間1"]
-                    hyozyun2=z_num2["標準時間2"]
-                    pvit["標準時間1"]=int(hyozyun1)
-                    pvit["標準時間2"]=int(hyozyun2)
+#                     hyozyun1=z_num2["標準時間1"]
+#                     hyozyun2=z_num2["標準時間2"]
+#                     pvit["標準時間1"]=int(hyozyun1)
+#                     pvit["標準時間2"]=int(hyozyun2)
                     graph_num=pd.concat([graph_num, pvit], axis=0)        
            
         
