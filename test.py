21b34922d256abdd1bb9ba99c7a006aa1f901285
mi-maker01 <106691573+mi-maker01@users.noreply.github.com>
2022-09-30 08:43:12 +0900

    Update test.py

--- test.py
+++ test.py
@@ -400,7 +400,7 @@ elif selector=="（C）同一行程内のばらつき把握_ヒストグラム":
         plt.grid(True)
         plt.axvline(x=int(hyozyun1),color = "crimson")#標準時間の表記（赤軸）
         plt.axvline(x=int(hyozyun2),color = "Blue")#標準時間の表記（軸）
-        plt.xticks(np.arange(lower_num2, upper_num2,dif_num2/10))
+        plt.xticks(np.arange(lower_num2, upper_num2,dif_num/10))
         labels = ax.get_xticklabels()
         plt.setp(labels, rotation=45, fontsize=10)
         ax.hist(zentai_dd,bins=10,range=(lower_num2,upper_num2),rwidth=dif_num2/10)
