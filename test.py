aaca1a84ff4e2baba1546236b1d0f679d6599444
mi-maker01 <106691573+mi-maker01@users.noreply.github.com>
2022-10-07 14:37:30 +0900

    Update test.py

--- test.py
+++ test.py
@@ -186,7 +186,7 @@ elif selector=="（A-2）各工程各日の実績ガントチャート":
 elif selector=="（B）同一人物の同一行程でのばらつきの把握_ヒストグラム":
     
     #曜日の設定
-    st.session_state.df["曜日"]=st.session_state.df["工程開始日"].dt.day_name
+    st.session_state.df["曜日"]=st.session_state.df["工程開始日"].dt.weekday
     #月の設定
     st.session_state.df["月"]=st.session_state.df["工程開始日"].dt.month
     #年の設定
