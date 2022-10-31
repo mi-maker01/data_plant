17d29e36e9586314b48deef9a6f8f41bc255dad8
mi-maker01 <106691573+mi-maker01@users.noreply.github.com>
2022-10-31 22:36:41 +0900

    Update test.py

--- test.py
+++ test.py
@@ -194,7 +194,7 @@ elif selector=="（B）同一人物の同一行程でのばらつきの把握_�
     st.session_state.df["年"]=st.session_state.df["工程開始日"].dt.year
     #時刻の設定
     st.session_state.df["時刻"]=st.session_state.df["開始日時"].dt.hour
-    if st.session_state.df["時刻"]=<7:
+    if st.session_state.df["時刻"]<=7:
         st.session_state.df["時刻"]=0
     elif st.session_state.df["時刻"]>=8 and st.session_state.df["時刻"]<=10:
         st.session_state.df["時刻"]=1
