e47d495635219261c1b8f5b4e40fcca979a587bd
mi-maker01 <106691573+mi-maker01@users.noreply.github.com>
2022-11-01 12:49:27 +0900

    Update test.py

--- test.py
+++ test.py
@@ -643,7 +643,7 @@ elif selector=="（D）一つの製品の総社内滞在時間の把握":
             d_start = d_start + datetime.timedelta(days=1)
         
         #滞在時間
-        zentai_list=pd.DataFrame(columns="総滞在時間")
+        zentai_list=pd.DataFrame()
         end_num=d_num[(d_num["工程名称"]=="配送")]
         sei_list = sorted(list(set(end_num["製造番号"])))
         for s in sei_list:
