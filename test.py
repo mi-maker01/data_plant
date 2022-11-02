4f9ace8b3cec04e9336d68b9c422de8fab94b15d
mi-maker01 <106691573+mi-maker01@users.noreply.github.com>
2022-11-02 14:42:09 +0900

    Update test.py

--- test.py
+++ test.py
@@ -856,10 +856,10 @@ elif selector=="（D）一つの製品の総社内滞在時間の把握":
                 sta_num.append(row.開始日時)
                 end_num.append(row.完了日時)
          
-            zentai_num=zentai_num.append(end_num[-1]-sta_num[0])
+            zentai_num=end_num[-1]-sta_num[0]
             
             
-        st.write(zentai_num)
+        st.write(zentai_num.type)
         #仕掛表
         pvit_data=pvit_data.T
         pvit_data=pvit_data.fillna(0)
