ee3c3f63b81eeb80e060869947f9578232e8081d
mi-maker01 <106691573+mi-maker01@users.noreply.github.com>
2022-11-15 11:50:18 +0900

    Update test.py

--- test.py
+++ test.py
@@ -158,7 +158,7 @@ elif selector=="　2.(ガントチャート)設備の空き":
     answer = st.button('分析開始')
     if answer == True:
         st.write("--------")
-        fig = go.Figure(px.timeline(d_num, x_start="開始日時", x_end="完了日時",text="処理時間",y="号機名称",color="号機名称",title="一日の稼働状況見える化"))
+        fig = go.Figure(px.timeline(d_num, x_start="開始日時", x_end="完了日時",text="処理時間",y="号機名称",color="号機名称",title="全体の稼働状況"))
         fig.update_traces(textposition='inside', orientation="h")
         st.plotly_chart(fig)
         
@@ -167,7 +167,7 @@ elif selector=="　2.(ガントチャート)設備の空き":
             k_num=k_num.sort_values(["開始日時"])
             if len(k_num) >=1:
                 st.write("--------")
-                st.write(k,":",len(k_num))
+                st.write(k,"の稼働状況")
 
                 fig = go.Figure(px.timeline(k_num, x_start="開始日時", x_end="完了日時",text="処理時間",y="担当者",color="担当者", color_continuous_scale='Jet',title="稼働状況の詳細"))
                 fig.update_traces(textposition='inside', orientation="h")
