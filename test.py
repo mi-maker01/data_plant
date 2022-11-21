104696d7d22058e5aa017f9cda838e109b9f0509
mi-maker01 <106691573+mi-maker01@users.noreply.github.com>
2022-11-22 03:37:43 +0900

    Update test.py

--- test.py
+++ test.py
@@ -870,7 +870,7 @@ elif selector=="　1.(折れ線)仕掛品の推移":
 
             num=pd.DataFrame(date_koutei_num.groupby(["工程名称"])['作成数'].agg(["count"]))
             
-            st.write(d_start)
+            st.write(str(d_start))
             pvit=num.set_axis([d_start], axis=1)
 #             st.write(pvit)
             pvit_data=pd.merge(pvit_data,pvit, right_index=True, left_index=True, how='outer')
@@ -905,12 +905,12 @@ elif selector=="　1.(折れ線)仕掛品の推移":
         
         #仕掛品の折れ線グラフ
         st.write("""#### 仕掛品の折れ線グラフ""")
-        fig = px.line(pvit_data,title="仕掛品の折れ線グラフ")
+        fig = px.line(pvit_data)
         st.plotly_chart(fig,use_container_width=True)
         
         #総社内滞在時間ガントチャート
         st.write("""#### 総社内滞在時間""")
-        fig = go.Figure(px.timeline(d_num, x_start="開始日時", x_end="完了日時",text="処理時間",y="製造番号",color="工程名称",title="総社内滞在時間"))
+        fig = go.Figure(px.timeline(d_num, x_start="開始日時", x_end="完了日時",text="処理時間",y="製造番号",color="工程名称"))
         fig.update_traces(textposition='inside', orientation="h")
         fig.update_yaxes(autorange='reversed')
         st.plotly_chart(fig,use_container_width=True)
